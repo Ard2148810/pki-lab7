a6321a3b52c31776d5e52b69286e128e09b954b5
Adrian Ałaszewski <wns2148810@interia.pl>
2020-05-03 14:40:33 +0200

    Using google keys

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 
 
-app.use(express.static('static'))
+app.use(express.static('static'));
+
+CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+REDIRECT_URL = process.env.GOOGLE_REDIRECT_URL;
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 app.listen(process.env.PORT, function () {
     console.log('Example app listening on port: ' + process.env.PORT);
+    console.log('Google OAuth data:\n' + CLIENT_ID + '\n' + CLIENT_SECRET + '\n' + REDIRECT_URL);
 });
