8f89086ff1165c40e59a53ebbf96647493d63fd1
Adrian Ałaszewski <wns2148810@interia.pl>
2020-05-03 18:20:31 +0200

    Added logging out

--- app.js
+++ app.js
@@ -14,14 +14,10 @@ var authed = false;
 
 app.get('/', (req, res) => {
     if (!authed) {
-        // Generate an OAuth URL and redirect there
-        const url = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: 'https://www.googleapis.com/auth/userinfo.profile'
-        });
-        console.log(url)
-        res.redirect(url);
+        console.log("Not logged in");
+        res.send('<a href="/login">Click here to login</a>');
     } else {
+        console.log("Logged in");
         const oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2' });
         oauth2.userinfo.v2.me.get(function (err, result) {
             if(err) {
@@ -32,10 +28,31 @@ app.get('/', (req, res) => {
             }
             res.send('Logged in: '.concat(loggedUser,
                 ' <img src="', result.data.picture,
-                '"height="23" width="23">'));
+                '"height="23" width="23">',
+                '<br><a href="/logout">Click here to logout</a>'));
         });
     }
-})
+});
+
+app.get('/logout', (req, res) => {
+    console.log('Loggin out');
+    authed = false;
+    res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+    if(!authed) {
+        const url = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: 'https://www.googleapis.com/auth/userinfo.profile'
+        });
+        console.log('Redirecting to: ' + url);
+        res.redirect(url);
+    } else {
+        console.log('Already logged in');
+        res.redirect('/');
+    }
+});
 
 app.get('/auth/google/callback', function (req, res) {
     const code = req.query.code
