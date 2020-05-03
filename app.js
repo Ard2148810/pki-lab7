9b5b4b1955cdc3b155fccad932b4d5df72306d3f
Adrian Ałaszewski <wns2148810@interia.pl>
2020-05-03 14:16:21 +0200

    Google verification

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-app.listen(process.env.PORT || 5000, function () {
-    console.log('Example app listening on port: ' + process.env.PORT || 5000);
+app.listen(process.env.PORT, function () {
+    console.log('Example app listening on port: ' + process.env.PORT);
 });
